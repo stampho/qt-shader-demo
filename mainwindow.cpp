d17542770f1358bad437a6369fa0051b7518cf79
Peter Varga <pvarga@inf.u-szeged.hu>
2015-03-30 23:40:33 +0200

    Add UI elements

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , m_ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    m_ui->setupUi(this);
+
+    m_ui->objectListWidget->addItem(QString("Object 1"));
+    m_ui->objectListWidget->addItem(QString("Object 2"));
+    m_ui->objectListWidget->addItem(QString("Object 3"));
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete m_ui;
 }
